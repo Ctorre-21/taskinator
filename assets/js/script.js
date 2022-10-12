444526f597b935876ab0770f13f99ce68209fd65
Chris Torres <ChrisTorres@Chriss-MacBook-Pro.local>
2022-10-11 21:53:31 -0400

    add task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var taskToDoEl = document.querySelector('#task-to-do');
 var createTaskHandler = function() {
     var listItemEl = document.createElement('li');
     listItemEl.textContent = 'This is a new task.';
-    listItemEl.className = 'task-item'
+    listItemEl.className = 'task-item';
     taskToDoEl.appendChild(listItemEl);
 };
 
