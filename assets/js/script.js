8842e4cee448ed768a2eff42ac57fb0c6e2d30c5
Chris Torres <ChrisTorres@Chriss-MacBook-Pro.local>
2022-10-12 00:59:39 -0400

    add error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#task-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
     event.preventDefault();
+    console.log(event);
 
   var listItemEl = document.createElement("li");
   listItemEl.textContent = "This is a new task.";
   listItemEl.className = "task-item";
-  taskToDoEl.appendChild(listItemEl);
+  taskToDoEl.appendChild(listItemEl); 
 };
 
 
